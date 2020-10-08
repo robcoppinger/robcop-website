9ed339e75a57a2b4288896e99609291a74297f2d
Rob Coppinger <coppinger.robert@gmail.com>
2020-10-08 23:10:43 +0100

    add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
 import React from "react";
 import "./css/base.css";
 import "./css/themes.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Home } from "./Pages/Home";
 
 function App() {
   return (
-      <div className="contentContainer">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
