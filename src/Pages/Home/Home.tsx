a04a1595343351844ded42ca23fbe469d2066e46
Rob Coppinger <coppinger.robert@gmail.com>
2020-10-10 22:30:32 +0100

    update home screen to use InfoCard

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,26 +1,17 @@
 import React from "react";
-import "./Home.css";
-import HeroImg from "../../assets/images/hero.jpg";
-import { Card } from "../../components/Card.tsx";
-import Headshot from "../../assets/images/headshot.jpg";
+import styles from "./Home.module.scss";
+import { InfoCard } from "../../components/InfoCard";
 
 export const Home = () => (
   <>
-    <img className="hero" src={HeroImg} />
-    <Card
-      style={{
-        borderTopLeftRadius: 0,
-        borderTopRightRadius: 0,
-        paddingTop: "12rem",
-        textAlign: "center",
-      }}
-    >
-      <div className="headshot">
-        <img src={Headshot} style={{ width: "100%" }}></img>
+    <div className={styles.hero}>
+      <div className={styles.desktopOnly}>
+        <InfoCard />
       </div>
-      <h1>Rob Coppinger</h1>
-      <h3 className='subtitle'>Software Developer</h3>
-    </Card>
+    </div>
+    <div className={styles.mobileOnly}>
+      <InfoCard />
+    </div>
     <div style={{ height: "20rem" }}></div>
   </>
 );
