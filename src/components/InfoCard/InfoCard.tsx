86ca95dd6cca40a5e1fc82dacd28e07815fad5a8
Rob Coppinger <coppinger.robert@gmail.com>
2020-10-10 22:29:39 +0100

    create InfoCard component

---
+++ src/components/InfoCard/InfoCard.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { images } from "../../assets/images";
+import styles from "./infoCard.module.scss";
+
+export const InfoCard = () => (
+  <div className={styles.infoCardContainer}>
+    <div className={styles.card}>
+      <div className={styles.infoCard}>
+        <div className={styles.headshot}>
+          <img src={images.Headshot} style={{ width: "100%" }}></img>
+        </div>
+        <div className={styles.titleContainer}>
+          <h1>Rob Coppinger</h1>
+          <h3 className={styles.subtitle}>Software Developer</h3>
+        </div>
+        <div className={styles.infoContainer}>
+          {infoData.map((item) => (
+            <div className={styles.infoRow}>
+              <img src={item.icon} />
+              <p>{item.text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+const infoData = [
+  {
+    icon: images.DummyIcon,
+    text: "University of Pretoria",
+  },
+  {
+    icon: images.DummyIcon,
+    text: "Full Stack Web / Mobile",
+  },
+  {
+    icon: images.DummyIcon,
+    text: "BSc Geoinformatics",
+  },
+  {
+    icon: images.DummyIcon,
+    text: "JavaScript / PHP",
+  },
+];
