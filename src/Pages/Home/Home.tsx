095f3e0d0842f4e2c7dafcf9f5d497110798390c
Rob Coppinger <coppinger.robert@gmail.com>
2020-10-27 23:24:19 +0000

    add theme swithing

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,23 +1,43 @@
 import React from "react";
 import styles from "./Home.module.scss";
 import { InfoCard } from "../../components/InfoCard";
-import { StackItem } from "../../components/StackItem";
 import { StackSection } from "../../components/StackSection";
 import { Footer } from "../../components/Footer";
 import { HobbiesSection } from "../../components/HobbiesSection";
 
-export const Home = () => (
-  <>
-    <div className={styles.hero}>
-      <div className={styles.desktopOnly}>
+export const Home = () => {
+  type Theme = "light" | "dark";
+  const dataTheme = () =>
+    document.getElementById("html")?.getAttribute("data-theme");
+
+  const setDataTheme = (theme: Theme) =>
+    document.getElementById("html")?.setAttribute("data-theme", theme);
+
+  function toggleTheme() {
+    setDataTheme(dataTheme() === "light" ? "dark" : "light");
+  }
+  return (
+    <>
+      <div className={styles.hero}>
+        <div className={styles.themeToggleContainer}>
+          <input
+            type="checkbox"
+            id="theme-switch"
+            name="theme"
+            onChange={toggleTheme}
+          />
+          <label htmlFor="theme-switch">Toggle</label>
+        </div>
+        <div className={styles.desktopOnly}>
+          <InfoCard />
+        </div>
+      </div>
+      <div className={styles.mobileOnly}>
         <InfoCard />
       </div>
-    </div>
-    <div className={styles.mobileOnly}>
-      <InfoCard />
-    </div>
-    <StackSection />
-    <HobbiesSection />
-    <Footer />
-  </>
-);
+      <StackSection />
+      <HobbiesSection />
+      <Footer />
+    </>
+  );
+};
