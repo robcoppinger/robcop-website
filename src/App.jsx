ccb0ebca8b097f67d5fd6c2a604e8b5670c715ea
Rob Coppinger <coppinger.robert@gmail.com>
2020-10-28 21:51:16 +0000

    add google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import ReactGA from "react-ga";
 import "./css/base.css";
 import "./css/themes.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Home } from "./Pages/Home";
+import { env } from "./env";
 
 function App() {
+  ReactGA.initialize(env.GoogleAnalyticsId);
+  ReactGA.pageview(window.location.pathname + window.location.search);
   return (
     <Router>
       <Switch>
